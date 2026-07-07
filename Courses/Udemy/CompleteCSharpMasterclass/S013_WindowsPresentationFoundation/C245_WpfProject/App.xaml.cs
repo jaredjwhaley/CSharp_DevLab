8c2f197ab036a001df640a401f67e507d605955a
Jared Whaley <jwhaley@ibml.com>
2026-07-07 15:23:37 -0500

    Created a new project for topics concerning WPF Framework applications.

---
+++ Courses/Udemy/CompleteCSharpMasterclass/S013_WindowsPresentationFoundation/C245_WpfProject/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace C245_WpfProject
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
